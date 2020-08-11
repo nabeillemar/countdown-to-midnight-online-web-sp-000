0ed7b3fd4debf658ea4d5ba14de23f56874ca2b2
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-11 13:49:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(num)
-  number = 10 
-  while number > 0
+  number = 1 
+  while number > 10
   puts "#{number} SECOND(S)!"
-  number += 1
+  number -= 1
   return "HAPPY NEW YEAR"
 end 
 end
